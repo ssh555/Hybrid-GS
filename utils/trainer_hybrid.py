4e5802f66a3e2383422e9b86298564507f63d89c
ssh555 <2099494912@qq.com>
2026-03-25 23:14:06 +0800

    A

--- utils/trainer_hybrid.py
+++ utils/trainer_hybrid.py
@@ -209,7 +209,7 @@ class TrainerHybrid(TrainerSWinGS):
                      self.robust_hard_constraint_classifier()
                 # ====================================================================
 
-                if iteration < self.opt.densify_until_iter and (self.opt.densify_until_num_points < 0 or self.gaussians.get_xyz.shape[0] < self.opt.densify_until_num_points):
+                if iteration < self.opt.densify_until_iter and (self.opt.densify_until_num_points < 0 or (self.gaussians.get_xyz.shape[0] + self.gaussians.get_static_xyz.shape[0] if static else 0) < self.opt.densify_until_num_points):
                     self.gaussians.max_radii2D[visibility_filter] = torch.max(self.gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
                     if static:
                         self.gaussians.static_max_radii2D[visibility_filter_static] = torch.max(self.gaussians.static_max_radii2D[visibility_filter_static], radii_static[visibility_filter_static])
