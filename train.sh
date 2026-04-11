de298ac997222ceb7356dc05aab8fd0818ed0c8d
ssh555 <2099494912@qq.com>
2026-04-11 15:41:20 +0800

    Update

--- train.sh
+++ train.sh
@@ -9,6 +9,18 @@ Full='Full'
 short='test_short'
 Short='Test'
 
+# Hybrid-GS SHORT
+for data in ${dataset[@]};
+do
+    model_path=output/hybrid_gs/${data}/${short}
+    source_path=datasets/${data}/${short}
+    # 在yaml配置文件中恢复了model_path和source_path的默认值，因此这里不需要再传入参数了。
+    CUDA_VISIBLE_DEVICES=${device} python3 train.py --config configs/${config}/HybridGS_${Short}.yaml  --model_path $model_path \
+    --source_path $source_path 
+    CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/HybridGS_${Short}.yaml  --model_path $model_path \
+    --source_path $source_path --start_checkpoint $model_path/chkpnt_8000.pth
+done
+
 # Hybrid-GS Full
 for data in ${dataset[@]};
 do
@@ -21,17 +33,17 @@ do
     --source_path $source_path --start_checkpoint $model_path/chkpnt_10000.pth
 done
 
-# # Hybrid-GS SHORT
-# for data in ${dataset[@]};
-# do
-#     model_path=output/hybrid_gs/${data}/${short}
-#     source_path=datasets/${data}/${short}
-#     # 在yaml配置文件中恢复了model_path和source_path的默认值，因此这里不需要再传入参数了。
-#     CUDA_VISIBLE_DEVICES=${device} python3 train.py --config configs/${config}/HybridGS_${Short}.yaml  --model_path $model_path \
-#     --source_path $source_path 
-#     CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/HybridGS_${Short}.yaml  --model_path $model_path \
-#     --source_path $source_path --start_checkpoint $model_path/chkpnt_8000.pth
-# done
+# SwinGS SHORT
+for data in ${dataset[@]};
+do
+    model_path=output/swings/${data}/${short}
+    source_path=datasets/${data}/${short}
+    # 在yaml配置文件中恢复了model_path和source_path的默认值，因此这里不需要再传入参数了。
+    CUDA_VISIBLE_DEVICES=${device} python3 train.py --config configs/${config}/SwinGS_${Short}.yaml  --model_path $model_path \
+    --source_path $source_path 
+    CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/SwinGS_${Short}.yaml  --model_path $model_path \
+    --source_path $source_path --start_checkpoint $model_path/chkpnt_8000.pth
+done
 
 # SwinGS Full
 for data in ${dataset[@]};
@@ -45,29 +57,7 @@ do
     --source_path $source_path --start_checkpoint $model_path/chkpnt_10000.pth
 done
 
-# # SwinGS SHORT
-# for data in ${dataset[@]};
-# do
-#     model_path=output/swings/${data}/${short}
-#     source_path=datasets/${data}/${short}
-#     # 在yaml配置文件中恢复了model_path和source_path的默认值，因此这里不需要再传入参数了。
-#     CUDA_VISIBLE_DEVICES=${device} python3 train.py --config configs/${config}/SwinGS_${Short}.yaml  --model_path $model_path \
-#     --source_path $source_path 
-#     CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/SwinGS_${Short}.yaml  --model_path $model_path \
-#     --source_path $source_path --start_checkpoint $model_path/chkpnt_8000.pth
-# done
 
-# # 3D4DGS Full
-# for data in ${dataset[@]};
-# do
-#     model_path=output/3d4dgs/${data}/${full}
-#     source_path=datasets/${data}/${full}
-#     # 在yaml配置文件中恢复了model_path和source_path的默认值，因此这里不需要再传入参数了。
-#     CUDA_VISIBLE_DEVICES=${device} python3 train.py --config configs/${config}/3D4DGS_${Full}.yaml  --model_path $model_path \
-#     --source_path $source_path 
-#     CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/3D4DGS_${Full}.yaml  --model_path $model_path \
-#     --source_path $source_path --start_checkpoint $model_path/chkpnt_40000.pth
-# done
 # # 3D4DGS SHORT
 # for data in ${dataset[@]};
 # do
@@ -78,5 +68,16 @@ done
 #     --source_path $source_path 
 #     CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/3D4DGS_${Short}.yaml  --model_path $model_path \
 #     --source_path $source_path --start_checkpoint $model_path/chkpnt_8000.pth
+# done
+# # 3D4DGS Full
+# for data in ${dataset[@]};
+# do
+#     model_path=output/3d4dgs/${data}/${full}
+#     source_path=datasets/${data}/${full}
+#     # 在yaml配置文件中恢复了model_path和source_path的默认值，因此这里不需要再传入参数了。
+#     CUDA_VISIBLE_DEVICES=${device} python3 train.py --config configs/${config}/3D4DGS_${Full}.yaml  --model_path $model_path \
+#     --source_path $source_path 
+#     CUDA_VISIBLE_DEVICES=${device} python3 render.py --config configs/${config}/3D4DGS_${Full}.yaml  --model_path $model_path \
+#     --source_path $source_path --start_checkpoint $model_path/chkpnt_40000.pth
 # done
 
